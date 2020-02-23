29b992db4b393f9db228bf37a0200c17147f9adb
Kamran Mmmdv <amorfrap@gmail.com>
2020-02-23 20:54:27 +0400

    Deleted static file dirs

--- pragmatechproject/settings.py
+++ pragmatechproject/settings.py
@@ -143,7 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = MEDIA_DIR
