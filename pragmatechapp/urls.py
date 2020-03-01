c1b699798b36c2892b8a9be86974f82f6bb7daf6
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-01 13:13:19 +0400

    Commented index url

--- pragmatechapp/urls.py
+++ pragmatechapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('about-us', views.about_us, name='about-us'),
     path('pragmatech-ustunlukler', views.advantages, name='pragmatech-ustunlukler'),
     path('tedris-paketleri', views.packages, name='tedris-paketleri'),
@@ -18,5 +18,5 @@ urlpatterns = [
     path('frontend-development', views.frontend, name='frontend-development'),
     path('backend-development', views.backend, name='backend-development'),
     path('contact', views.contact, name='contact'),
-    # path('', views.coming_soon, name='coming-soon')
+    path('', views.coming_soon, name='coming-soon')
 ]
