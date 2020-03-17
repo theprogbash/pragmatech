27c254de7833ae5b8f59b84d9bf6501701ec32c1
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-17 10:17:16 +0400

    Added js for success

---
+++ static/assets/js/success.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".close-btn").click(function () {
+        $(".success-overlay").fadeOut();
+        $(".success-overlay").fadeOut(1000);
+    });
+
+    $(document).click(function (e) {
+        if ($(e.target).is('.apply-overlay')) {
+            $(".success-overlay").fadeOut();
+            $(".asuccesspply-overlay").fadeOut(1000);
+        }
+    });
+});
