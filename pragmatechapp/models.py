f9edd972655a2dfcd2192ecd378a156aca1e38e4
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-28 10:31:25 +0400

    Added date to both applicant and message models

--- pragmatechapp/models.py
+++ pragmatechapp/models.py
@@ -4,7 +4,7 @@ class Applicant(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=30)
-    
+    date_applied = models.DateTimeField(auto_now_add = True)
     def __str__(self):
         return self.name
 
@@ -13,6 +13,6 @@ class Message(models.Model):
     sender_email = models.EmailField(max_length=50)
     message_subject = models.CharField(max_length=100)
     message_content = models.CharField(max_length=300)
-
+    date_sent = models.DateTimeField(auto_now_add = True)
     def __str__(self):
         return self.message_subject
