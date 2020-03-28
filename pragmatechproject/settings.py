d4065b81eb589c8aea52d4a060031381515e1da7
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-28 10:29:40 +0400

    Changed language code and timezone

--- pragmatechproject/settings.py
+++ pragmatechproject/settings.py
@@ -128,9 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'az'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Baku'
 
 USE_I18N = True
 
