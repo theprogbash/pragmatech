ac56a475f418af0edb5185533acf97d9aba8d9ef
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-28 10:44:16 +0400

    Added dates back

--- pragmatechapp/admin.py
+++ pragmatechapp/admin.py
@@ -3,10 +3,10 @@ from .models import Applicant, Message
 from django.contrib.auth.models import Group, User
 
 class ApplicantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone')
+    list_display = ('name', 'email', 'phone', 'date_applied')
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender_name', 'sender_email', 'message_subject', 'message_content')
+    list_display = ('sender_name', 'sender_email', 'message_subject', 'message_content', 'date_sent')
 
 admin.site.site_header = "Pragmatech - Admin Dashboard" 
 admin.site.unregister(Group)
