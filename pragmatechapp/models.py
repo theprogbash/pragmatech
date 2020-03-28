55ea08a8d04710964a4f6c6fe2230699bf9a6d63
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-28 11:47:27 +0400

    Added blank=True to date

--- pragmatechapp/models.py
+++ pragmatechapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Applicant(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=30)
-    date_applied = models.DateTimeField(auto_now_add=True)
+    date_applied = models.DateTimeField(auto_now_add=True, blank=True)
     def __str__(self):
         return self.name
 
@@ -13,6 +14,6 @@ class Message(models.Model):
     sender_email = models.EmailField(max_length=50)
     message_subject = models.CharField(max_length=100)
     message_content = models.CharField(max_length=300)
-    date_sent = models.DateTimeField(auto_now_add=True)
+    date_sent = models.DateTimeField(auto_now_add=True, blank=True)
     def __str__(self):
         return self.message_subject
